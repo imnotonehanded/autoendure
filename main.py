f232bf6e6e5727d3f8f88d9e4fdb8521dc44b067
airttq <airttq@gmail.com>
2022-02-01 19:48:21 -0500

    update

--- main.py
+++ main.py
@@ -37,26 +37,64 @@ def start():
   global ssd
   ssd+=1
   print('\033[2;32mStarted endurance '+str(ssd))
-  if prints: print("Clicking on map button")
-  #find map.png with a confidence of 0.8, click it
+
+
+  loc0 = pyautogui.locateOnScreen('D:/autoendure-1/images/map.png', confidence = 0.8)
+  if loc0 !=None:
+    pyautogui.click(loc0)
+  time.sleep(1)
+
+
+  loc1 = pyautogui.locateOnScreen('D:/autoendure-1/images/'+zonepng, confidence = 0.8)
+  if loc1 !=None:
+    pyautogui.click(loc1)
+    pyautogui.click(loc1)
+    pyautogui.click(loc1)
+  time.sleep(1)
+
+  loc2 = pyautogui.locateOnScreen('D:/autoendure-1/images/ssd.png', confidence = 0.8)
+  if loc2 !=None:
+    pyautogui.click(loc2)
+    pyautogui.click(loc2)
+    pyautogui.click(loc2)
   time.sleep(0.5)
-  if prints: print("Loading into zone")
-  #gets zone from the config file, double clicks
+  
+
+  loc3 = pyautogui.locateOnScreen('D:/autoendure-1/images/luanch.png', confidence = 0.8)
+  if loc3 !=None:
+    time.sleep(0.5)
+    pyautogui.click(loc3)
+    pyautogui.click(pyautogui.locateOnScreen('D:/autoendure-1/images/distract.png', confidence = 0.8))
+    pyautogui.click(loc3)
+    
+  time.sleep(15)
+  loc4 = pyautogui.locateOnScreen('D:/autoendure-1/images/luanch2.png', confidence = 0.8)
+  if loc4 !=None:
+    pyautogui.click(loc4)
   time.sleep(0.5)
-  if prints: print("Locating and loading into storm shield")
-  #waits for the luanch buttons then presses on it 
-  time.sleep(0.5)
+
+
   if prints: print("Luanched")
   time.sleep(0.5)
+
+
   if prints: print("Waiting for ssd to load in")
   time.sleep(0.5)
+
+
   #waits until ssdtext.png is not visible
   if prints: print("Walking to storm computer")
   #uses keystrokes to simulate keypresses, once ended press e, then after 1 second finds storm shield buttons and presses it then finds start and starts 
+
+
   if prints: print("Started, waiting for endurance to end")
   #every 10 seconds it waits to find the open buttons, presses it then presses collect all
+
+
   if prints: print("Collected Chest")
   #checks again if it can see open, if it does it opens then collect all, if it doesnt then it starts again
+
+
   start()
   
 
@@ -78,11 +116,11 @@ if checkKey(enteredKey):
   2. Make sure that the config file is filled out with the zone, it will not work if it doesn't know what zone to load into
   3. Also make sure to have the macro file that has the keystrokes to walk to the storm sheild computer, if it doesnt go to the computer it can't start
   """)
-  choice = input("Start? y/n")
+  choice = input("Start? y/n: ")
   if str(choice) == "y":
     print(choice)
     cls()
-    for i in reversed(range(1)):
+    for i in reversed(range(2)):
       print("Starting in " + str(i+1))
       time.sleep(1)
       cls()
--- testing.py
+++ testing.py
@@ -5,14 +5,12 @@ import keyboard
 import random
 import win32api, win32con
 
+time.sleep(2)
 
-def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
-
-if pyautogui.locateOnScreen('D:/autoendure/images/map.png', confidence = 0.8) !=None:
+loc0 = pyautogui.locateOnScreen('D:/autoendure-1/images/map.png', confidence = 0.8)
+if loc0 !=None:
     print("seen")
+    pyautogui.click(loc0)
     time.sleep(0.5)
 else:
     print("not seen")
